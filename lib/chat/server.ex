fb5bc213928490dad788055ea45bb2aad375ed75
richard12511 <richardnew12511@gmail.com>
2018-11-26 19:55:57 -0600

    initial commit

---
+++ lib/chat/server.ex
@@ -0,0 +1,26 @@
+defmodule Chat.Server do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def add_message(pid, message) do
+    GenServer.cast(pid, {:add_message, message})
+  end
+
+  def get_messages(pid) do
+    GenServer.call(pid, :get_messages)
+  end
+
+  #server
+  def init(messages), do: {:ok, messages}
+
+  def handle_cast({:add_message, new_message}, messages) do
+    {:noreply, [new_message | messages]}
+  end
+
+  def handle_call(:get_messages, _from, messages) do
+    {:reply, messages, messages}
+  end
+end
