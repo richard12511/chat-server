4dd74d1dce24f1d2a1bf7f249d251ea0aa1b5a03
richard12511 <richardnew12511@gmail.com>
2018-11-26 21:58:09 -0600

    add monitor

--- lib/chat/server.ex
+++ lib/chat/server.ex
@@ -1,16 +1,16 @@
 defmodule Chat.Server do
   use GenServer
 
-  def start_link do
-    GenServer.start_link(__MODULE__, [])
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, [], name: via_tuple(name))
   end
 
-  def add_message(pid, message) do
-    GenServer.cast(pid, {:add_message, message})
+  def add_message(room_name, message) do
+    GenServer.cast(via_tuple(room_name), {:add_message, message})
   end
 
-  def get_messages(pid) do
-    GenServer.call(pid, :get_messages)
+  def get_messages(room_name) do
+    GenServer.call(via_tuple(room_name), :get_messages)
   end
 
   #server
@@ -22,5 +22,9 @@ defmodule Chat.Server do
 
   def handle_call(:get_messages, _from, messages) do
     {:reply, messages, messages}
+  end
+
+  defp via_tuple(room_name) do
+    {:via, Chat.Registry, {:chat_room, room_name}}
   end
 end
